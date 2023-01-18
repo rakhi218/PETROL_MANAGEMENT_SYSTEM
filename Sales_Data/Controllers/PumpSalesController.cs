a39b9598ef354b7a44fffc4cbe6c56dfe9160151
Rakesh Vanga <rakesh_vanga@persistent.com>
2023-01-18 23:35:24 +0530

    Crated APIs of sales of petrol to update it in the database

---
+++ Sales_Data/Controllers/PumpSalesController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sales_Data.DataContext;
+using Sales_Data.Models;
+using Sales_Data.Services;
+using Pump_Data.Models;
+using System.Text.Json;
+
+namespace Sales_Data.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PumpSalesController : Controller
+    {
+        PumpSalesService pumpSalesService; 
+        public PumpSalesController(SalesDBContext salesDBContext) 
+        {
+            pumpSalesService = new PumpSalesService(salesDBContext);
+        }
+
+        [HttpPost]
+        [Route("CreatePumpRecordEntry")]
+        public IActionResult CreatePumpRecordEntry(PumpRecord pumpRecord)
+        {
+            try
+            {
+                JsonResponse jsonResponse = new JsonResponse();
+                bool status = pumpSalesService.CreatePumpRecordEntry(pumpRecord);
+                if (status)
+                {
+                    jsonResponse.Result = true;
+                    jsonResponse.Message = "Updated Pump Type and Pump Record Table";
+                }
+                else
+                {
+                    jsonResponse.Result = false;
+                    jsonResponse.Message = "Failed to update tables";
+                }
+                return Ok(jsonResponse);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        [Route("CreateSalesEntry")]
+        public IActionResult CreateSalesEntry(SalesRecord salesRecord)
+        {
+            try
+            {
+                JsonResponse jsonResponse = new JsonResponse();
+                bool status = pumpSalesService.CreateSalesEntry(salesRecord);
+                if (status)
+                {
+                    jsonResponse.Result = true;
+                    jsonResponse.Message = "Updated Sales Record Table";
+                }
+                else
+                {
+                    jsonResponse.Result = false;
+                    jsonResponse.Message = "Failed to update tables";
+                }
+                return Ok(jsonResponse);
+            }catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
